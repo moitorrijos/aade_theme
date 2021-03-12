ec57e4626e68163a11bc13e0978fa54c38d9815c
caizprua1996 <80171107+caizprua1996@users.noreply.github.com>
2021-03-12 12:06:07 -0500

    Crear container para logo Crear navbar (web) Crear navbar-responsibe (en proceso) Corregir error en global.css, @charset Agregar logo AADE en images como guia Agregar burger_menu.svg para crear navbar responsive

--- header.php
+++ header.php
@@ -2,8 +2,6 @@
 <?php wp_head(); ?>
 </head>
 <body>
-
-
 <header>
     <div class="top-bar">
         <div class="address-tel">
@@ -18,4 +16,24 @@
         </div>
         <div class="social-media"></div>
     </div>
+    <div class="container-fluid">
+        <div class="logo p-t-25 p-b-20">
+            <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/African_American_Dance_Ensemble1.svg" alt="logo"></a>
+        </div>
+    </div>
+    <nav class="upper-text paragraph-small-text p-all-15 m-t-10 m-b-10">
+        <ul>
+            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">aade - home</a></li>
+            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">our history</a></li>
+            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">support aade</a></li>
+            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">we can dance for you!!!</a></li>
+            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">what's new & upcoming!!</a></li>
+            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">product</a></li>
+        </ul>
+    </nav>
+    <div class="menu-toogle">
+        <div class="burger-menu">
+            <img src="<?php echo get_template_directory_uri();?>/images/burger_menu.svg" alt="menu">
+        </diV>
+    </div> 
 </header>
