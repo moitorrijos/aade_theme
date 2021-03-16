1d6ac01ead1e96ba6aa2324b4a2c0d456ab4a083
moitorrijos <juanmtorrijos@gmail.com>
2021-03-15 21:22:50 -0500

    Agrega el navbar y el logo.

--- header.php
+++ header.php
@@ -5,17 +5,38 @@
 
 
 <header>
-    <div class="top-bar">
-        <div class="address-tel">
-            <div class="address">
+    <div class="top-bar d-flex flex-row">
+        <div class="address-tel list-inline">
+            <div class="address list-inline-item">
                 <img src="<?php echo get_template_directory_uri();?>/images/pin.svg" alt="address-pin-icon">
                 <p>120 Morris St. Durham, NC 27701</p>
             </div>
-            <div class="tel">
+            <div class="tel d-flex flex-row">
                 <img src="<?php echo get_template_directory_uri();?>/images/phone.svg" alt="address-pin-icon">
                 <p>919-560-2729</p>
             </div>
         </div>
         <div class="social-media"></div>
     </div>
+    
+    <div class="mx-auto logo"></div>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-blue-light">
+      <button class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
 </header>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <body>
+
 <h1><?php bloginfo( 'description' ); ?></h1>
 </body>
 <?php get_footer(); ?>
