36f2571c9bc32275ea39626c2c239d4a2e753d14
moitorrijos <juanmtorrijos@gmail.com>
2021-03-15 20:53:14 -0500

    Arregla error en functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 if ( ! defined( 'AADE_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'AADE_VERSION', '1.0.0' );
+	define( 'AADE_VERSION', wp_get_theme()->get('Version') );
 }
 
 
@@ -11,7 +11,7 @@ add_action('wp_enqueue_scripts', 'aade_enqueue_styles');
 function aade_enqueue_styles(){
     wp_enqueue_style( 'aade_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css', array(), AADE_VERSION, 'all' );
     wp_enqueue_style( 'main_style', get_template_directory_uri() . '/css/main.css', array('aade_bootstrap'), AADE_VERSION, 'all' );
-    wp_enqueue_script( 'aade_bootstrap_js', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js', array('jquery'), $ver:string|boolean|null, $in_footer:boolean )
+    wp_enqueue_script( 'aade_bootstrap_js', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js', array('jquery'), AADE_VERSION, true );
 }
 
 add_action( 'after_setup_theme', 'aade_setup' );
