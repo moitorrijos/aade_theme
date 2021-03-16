3e7cecf7e6f528168c919c7e7b5070018123d79c
caizprua1996 <80171107+caizprua1996@users.noreply.github.com>
2021-03-16 00:08:18 -0500

    Modifica Navbar con parámetros de Bootsrap

--- header.php
+++ header.php
@@ -21,19 +21,31 @@
             <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/African_American_Dance_Ensemble1.svg" alt="logo"></a>
         </div>
     </div>
-    <nav class="upper-text paragraph-small-text p-all-15 m-t-10 m-b-10">
-        <ul>
-            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">aade - home</a></li>
-            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">our history</a></li>
-            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">support aade</a></li>
-            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">we can dance for you!!!</a></li>
-            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">what's new & upcoming!!</a></li>
-            <li class="m-l-10 m-r-10 p-t-15 p-b-10"><a href="#">product</a></li>
-        </ul>
-    </nav>
-    <div class="menu-toggle">
-        <div class="burger-menu">
-            <img src="<?php echo get_template_directory_uri();?>/images/burger_menu.svg" alt="menu">
-        </diV>
-    </div> 
+    <nav class="navbar navbar-expand-lg navbar-light bg-blue-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto text-uppercase">
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> aade - home</a>
+                </li>
+                <li class="nav-item">
+                     <a class="nav-link" href="#">our history</a>
+                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> support aade</a>
+                 </li>
+                <li class="nav-item">
+                     <a class="nav-link" href="#">we can dance for you!!!</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" href="#">what's new & upcoming!!</a>
+                 </li>
+                <li class="nav-item">
+                     <a class="nav-link" href="#">product</a>
+                </li>
+            </ul>
+        </div>
+    </nav> 
 </header>
