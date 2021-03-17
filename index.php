17b88739169953a6513dfcf92a5163018111dc2a
caizprua1996 <80171107+caizprua1996@users.noreply.github.com>
2021-03-16 20:51:53 -0500

    Estructura home en index.php

--- index.php
+++ index.php
@@ -1,5 +1,120 @@
 <?php get_header(); ?>
 <body>
-<h1><?php bloginfo( 'description' ); ?></h1>
+    <!--<h1><?php bloginfo( 'description' ); ?></h1>-->
+    <!--<div class="container-fluid bg-primary">
+        <div class="row align-items-center no-gutters">
+            <div class="col-xl-5 col-lg-6 col-md-12">
+                <div class="py-6 py-lg-0">
+                    <p class="text-white-50 mb-4 lead">
+                    Our Motto:
+                    </p>
+                    <h1 class="text-white display-4 font-weight-bold">
+                    PEACE! LOVE!
+                    RESPECT!
+                    FOR EVERYBODY
+                    </h1>
+                    <a href="#" class="btn btn-secondary btn-lg">Donate</a>
+                </div>
+            </div>
+           <div class="col-xl-7 col-lg-6 col-md-12 text-lg-right text-center">
+                <img src="<?php echo get_template_directory_uri();?>/images/aade-2_orig.png" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>-->
+   <main>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="our-mission-text-column bg-gray-100 col-lg-6">
+                        <div class="our-mission-text py-5">
+                            <h2 class="text-muted letter-spacing-5 my-4">Our Mission</h2>
+                            <p>The African American Dance Ensemble’s mission is to preserve
+                             and share the finest traditions of African and African American
+                              dance and music through research, education and
+                               entertainment. With the motto, “Peace, love, respect for
+                                everybody,” the Ensemble celebrates traditional African culture,
+                                 aesthetics and values as resources to encourage interracial
+                                  cooperation and cross-cultural understanding. Towards this end,
+                                   the Ensemble develops partnerships and collaborations with
+                                    community programs and resources, conducts residencies
+                                     and workshops in diverse settings, and offers apprenticeships
+                                      with emerging performers to enhance their artistic skills and
+                                       capacities.</p>
+                        </div>
+                    </div>
+                    <div class="p-lg-0 our-history-image-column col-lg-6">
+                        <div style="display: block; overflow: hidden; position: absolute; inset: 0px; box-sizing: border-box; margin: 0px;">
+                            <img scr="<?php echo get_template_directory_uri();?>/images/aade-2_orig.png" alt="#">
+                        </div>
+                    </div>
+                </div>
+            </div>-->
+        </section>
+        <section>
+            <div class="container py-5">
+                <h2 class="text-center">The African American Dance Ensemble uses dance and music to bring awareness to the inequities in our community and through our ART, createa positive change.</h2>
+            </div>
+        </section>
+        <section>
+            <div class="container py-5">
+            <h2 class="text-center">Performing arts * Health equity * Youth empowerment *</h2>
+            <div class="row text-center">
+                <div class="col-lg-3">
+                    <h6 class="text-uppercase">Dance with a purpose</h6>
+                    <p class="text-lg text-muted mb-lg-0">We dance for peace, we dance for strength.
+                    School performances, Board Trainings, Culture Festivals, and many more is where we can enhance community through the Arts.</p>
+                </div>
+                <div class="col-lg-3">
+                    <h6 class="text-uppercase">A healthy you</h6>
+                    <p class="text-lg text-muted mb-lg-0">Through African Dance, learn to become at peace with your body, mind and soul.
+                    A healthy you is a happy you!</p>
+                </div>
+                <div class="col-lg-3">
+                    <h6 class="text-uppercase">Our youth is our future</h6>
+                    <p class="text-lg text-muted mb-lg-0">Teaching our children Peace, Love, Respect, today, for Everybody, is creating a better world through the Arts.</p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div>
+                <div class="container">
+                    <div class="row">
+                        <h2 class="text-center">Building Stronger Communities through the power of the ARTS!</h2>
+                        <div class="foundation-column col-lg-3">
+                            <img scr="<?php echo get_template_directory_uri();?>/images/dac-logo2.gif" alt="#">
+                        </div>
+                        <div class="foundation-column col-lg-3">
+                            <img scr="<?php echo get_template_directory_uri();?>/images/uaclogo.jpg" alt="#">
+                        </div>
+                        <div class="foundation-column col-lg-3">
+                            <img scr="<?php echo get_template_directory_uri();?>/images/ncartslogo.jpg" alt="#">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container bg-blue-light">
+            <div class="row">
+                    <div class="p-lg-0 our-history-image-column col-lg-6">
+                        <div style="display: block; overflow: hidden; position: absolute; inset: 0px; box-sizing: border-box; margin: 0px;">
+                            <img scr="<?php echo get_template_directory_uri();?>/images/aade-2_orig.png" alt="#">
+                        </div>
+                    </div>
+                    <div class="our-mission-text-column bg-gray-100 col-lg-6">
+                        <div class="our-mission-text py-5">
+                            <h2 class="text-muted letter-spacing-5 my-4">Dancers for peace</h2>
+                            <p>AADE is committed to OUTREACH, focusing on the well-being and a possitive future of OUR Youth.
+                            We continue the Legacy of Dr. Chuck Davis in presserving, teaching and performing traditional African Dance Music and Values.
+                            We present dance and music from West African Dance, Music and Values.
+                            We present dance and music from West African Region as well as Congolese Drumming.
+                            Culture, History and Values that promote Unity and Diversity are in all our performances. (Call office for Fee Schedule)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+   </main>
 </body>
 <?php get_footer(); ?>
