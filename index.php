2663364a1790e3cebe6ba0170681d93c3d6c96f7
caizprua1996 <80171107+caizprua1996@users.noreply.github.com>
2021-03-16 22:55:35 -0500

    Corrige secciones en index.php Corrige atributos actuales y crea nuevos en main.css

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <body>
-    <!--<h1><?php// bloginfo( 'description' ); ?></h1>-->
+    <!--<h1><?php// bloginfo( 'desrciption' ); ?></h1>-->
     <!--<div class="container-fluid bg-primary">
         <div class="row align-items-center no-gutters">
             <div class="col-xl-5 col-lg-6 col-md-12">
@@ -17,15 +17,28 @@
                 </div>
             </div>
            <div class="col-xl-7 col-lg-6 col-md-12 text-lg-right text-center">
-                <img src="<?php// echo get_template_directory_uri();?>/images/aade-2_orig.png" alt="" class="img-fluid">
+                <img src="<?php echo get_template_directory_uri();?>/images/aade-2_orig.png" alt="" class="img-fluid">
             </div>
         </div>
     </div>-->
-    <div class="">
+    <div class="hero-image row align-items-center no-gutters">
+        <div class="container">
+            <div class="hero-text col-xl-5 col-lg-6 col-md-12">
+                <p class="text-white mb-4 lead our-motto">
+                Our Motto:
+                </p>
+                <h1 class="text-white display-4 font-weight-bold lh-1">
+                        PEACE! LOVE!
+                        <br>RESPECT!
+                        <br>FOR EVERYBODY
+                </h1>
+
+            </div>
+        </div>
     </div>
    <main>
         <section>
-            <div class="container">
+            <div class="container pt-5">
                 <div class="row">
                     <div class="our-mission-text-column bg-gray-100 col-lg-6">
                         <div class="our-mission-text py-5">
@@ -45,51 +58,49 @@
                         </div>
                     </div>
                     <div class="p-lg-0 our-history-image-column col-lg-6">
-                        <div style="display: block; overflow: hidden; position: absolute; inset: 0px; box-sizing: border-box; margin: 0px;">
-                            <img scr="<?php echo get_template_directory_uri();?>/images/aade-2_orig.png" alt="#">
-                        </div>
+                        <img src="<?php echo get_template_directory_uri();?>/images/aade-cropwalk-1_orig.jpg" alt="#">
                     </div>
                 </div>
             </div>-->
         </section>
-        <section>
+        <section class="bg-white">
             <div class="container py-5">
                 <h2 class="text-center">The African American Dance Ensemble uses dance and music to bring awareness to the inequities in our community and through our ART, createa positive change.</h2>
             </div>
         </section>
         <section>
             <div class="container py-5">
-            <h2 class="text-center">Performing arts * Health equity * Youth empowerment *</h2>
+            <h2 class="text-center pb-5">Performing arts * Health equity * Youth empowerment *</h2>
             <div class="row text-center">
-                <div class="col-lg-3">
+                <div class="col-lg-4">
                     <h6 class="text-uppercase">Dance with a purpose</h6>
                     <p class="text-lg text-muted mb-lg-0">We dance for peace, we dance for strength.
                     School performances, Board Trainings, Culture Festivals, and many more is where we can enhance community through the Arts.</p>
                 </div>
-                <div class="col-lg-3">
+                <div class="col-lg-4">
                     <h6 class="text-uppercase">A healthy you</h6>
                     <p class="text-lg text-muted mb-lg-0">Through African Dance, learn to become at peace with your body, mind and soul.
                     A healthy you is a happy you!</p>
                 </div>
-                <div class="col-lg-3">
+                <div class="col-lg-4">
                     <h6 class="text-uppercase">Our youth is our future</h6>
                     <p class="text-lg text-muted mb-lg-0">Teaching our children Peace, Love, Respect, today, for Everybody, is creating a better world through the Arts.</p>
                 </div>
             </div>
         </section>
-        <section>
+        <section class="bg-white">
             <div>
                 <div class="container">
                     <div class="row">
                         <h2 class="text-center">Building Stronger Communities through the power of the ARTS!</h2>
-                        <div class="foundation-column col-lg-3">
-                            <img scr="<?php echo get_template_directory_uri();?>/images/dac-logo2.gif" alt="#">
+                        <div class="foundation-column col-lg-4">
+                            <img src="<?php echo get_template_directory_uri();?>/images/dac-logo2.gif" alt="#">
                         </div>
-                        <div class="foundation-column col-lg-3">
-                            <img scr="<?php echo get_template_directory_uri();?>/images/uaclogo.jpg" alt="#">
+                        <div class="foundation-column col-lg-4">
+                            <img src="<?php echo get_template_directory_uri();?>/images/uaclogo.jpg" alt="#">
                         </div>
-                        <div class="foundation-column col-lg-3">
-                            <img scr="<?php echo get_template_directory_uri();?>/images/ncartslogo.jpg" alt="#">
+                        <div class="foundation-column col-lg-4">
+                            <img src="<?php echo get_template_directory_uri();?>/images/ncartslogo.jpg" alt="#">
                         </div>
                     </div>
                 </div>
@@ -100,7 +111,7 @@
             <div class="row">
                     <div class="p-lg-0 our-history-image-column col-lg-6">
                         <div style="display: block; overflow: hidden; position: absolute; inset: 0px; box-sizing: border-box; margin: 0px;">
-                            <img scr="<?php echo get_template_directory_uri();?>/images/aade-2_orig.png" alt="#">
+                            <img src="<?php echo get_template_directory_uri();?>/images/aade-2_orig.png" alt="#">
                         </div>
                     </div>
                     <div class="our-mission-text-column bg-gray-100 col-lg-6">
