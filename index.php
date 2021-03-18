e2ea9e84e0926e9e5a3954b3fea005f1f2aeaa6c
caizprua1996 <80171107+caizprua1996@users.noreply.github.com>
2021-03-17 21:13:36 -0500

    restarura index.php

--- index.php
+++ index.php
@@ -1,104 +1,113 @@
 <?php get_header(); ?>
 <body>
-    <div class="hero-image row align-items-center no-gutters our-history-hero-image">
+<div class="hero-image row align-items-center no-gutters">
         <div class="container">
-            <div class="hero-text mx-auto text-center">
-                <h1 class="text-white text-uppercase my-4 lead our-history-text">
-                Our history
+            <div class="hero-text col-xl-5 col-lg-6 col-md-12">
+                <p class="text-white mb-4 lead our-motto">
+                Our Motto:
+                </p>
+                <h1 class="text-white display-4 font-weight-bold lh-1">
+                        PEACE! LOVE!
+                        <br>RESPECT!
+                        <br>FOR EVERYBODY
                 </h1>
+                <a href="#" class="btn btn-lg button-hero">Donate</a>
+                <a href="#" class="btn btn-lg button-hero performance-tooltip"><span>Performance</span>Pay now</a>
             </div>
         </div>
     </div>
-    <main>
-        <section class="pt-5">
-            <div class="container">
+   <main>
+        <section>
+            <div class="container pt-5">
                 <div class="row">
-                    <div class="p-lg-0 col-lg-6">
-                        <img scr="#" alt="Dr. Charles Chuck Davis" class="avatar">
-                        <h2>Dr. Charles "Chuck" Davis</h2>
-                        <p>Jan 1, 1937 - May 14, 2017</p>
-                        <p>Refusing to become one of those who forget, We are reminded, that we are standing, marching, kneeling and existing by the grace and will of the Merciful Creator and to give true obeisance to HIM, is to NOT forget the warriors of the past, and the Elders of today.
-                        </p>
-                        <h2>AADE Founding Members</h2>
-                        <p>Dr. Charles "Chuck" Davis - Founder</p>
-                        <h2 class="text-uppercase">Founding Dancers</h2>
-                        <ul class="members">
-                            <li>Venita (Ashford) Allen</li>
-                            <li>Gloria (Muldrow) Bailey (past title: Dance Capt)</li>
-                            <li>Ivy Burch (past title: Dance Capt)</li>
-                            <li>Lou Carter</li>
-                            <li>Toni K. Hall (prior title: Dance Capt)</li>
-                            <li>Brenda Hayes (past title: Costumes/ Attire)</li>
-                            <li>Gail (Martin) Rouse</li>
-                            <li>Ava (McFarland) Vinesett (past title: Assistant to the Director)</li>
-                            <li>Leah Wise</li>
-                            <li>Thaddeus Bennett</li>
-                            <li>Lavender “L.D.” Burris</li>
-                            <li>Jimmie Green</li>
-                            <li>Sherone Price</li>
-                            <li>Jimmy Williams</li>
-                        </ul>
-                        <h2 class="text-uppercase">Founding musicians</h2>
-                        <ul class="members">
-                            <li>Beverly Botsford</li>
-                            <li>George Glenn</li>
-                            <li>Khalid Saleem (past title: Musical Director)</li>
-                            <li>Hashim Saleh</li>
-                        </ul>
-                        <h2>Technical Director/Lighting Designer</h2>
-                        <ul class="members">
-                            <li>Elizabeth Grimes Droessler</li>
-                        </ul>
+                    <div class="our-mission-text-column bg-gray-100 col-lg-6">
+                        <div class="our-mission-text py-5">
+                            <h2 class="text-muted letter-spacing-5 my-4">Our Mission</h2>
+                            <p>The African American Dance Ensemble’s mission is to preserve
+                             and share the finest traditions of African and African American
+                              dance and music through research, education and
+                               entertainment. With the motto, “Peace, love, respect for
+                                everybody,” the Ensemble celebrates traditional African culture,
+                                 aesthetics and values as resources to encourage interracial
+                                  cooperation and cross-cultural understanding. Towards this end,
+                                   the Ensemble develops partnerships and collaborations with
+                                    community programs and resources, conducts residencies
+                                     and workshops in diverse settings, and offers apprenticeships
+                                      with emerging performers to enhance their artistic skills and
+                                       capacities.</p>
+                        </div>
+                    </div>
+                    <div class="p-lg-0 our-history-image-column col-lg-6">
+                        <img src="<?php echo get_template_directory_uri();?>/images/aade-cropwalk-1_orig.jpg" alt="#">
                     </div>
                 </div>
-                <div class="row">
-                    <div class="p-lg-0 col-lg-6">
-                        <img scr="#" alt="Dr. B. Angeloe Burch Sr." class="avatar">
-                        <h2>Dr. B. Angeloe Burch Sr.</h2>
-                        <p>Dr. Burch currently serves as Executive Director for the African American Dance Ensemble. He is a State Commissioner for the NC- Credit Unions, Member of the NC Museum of Arts Board of Trustees, on the Citizen Advisory Committee and Homeless Services Advisory Committee is Secretary of the Inter-Denominational Ministerial Alliance of Durham and Vicinity. A life of service and commitment to the community and to mankind is the most fulfilling thing any person could wish.
-                            Dr. Burch is the Board Chair for CHTOP, Member of the Access to Care Committee, a member of the National Kidney Foundation, Member of Red Mt. Lodge #468 of the Prince Hall Masons and many other civic organizations.
-                        </p>
-                    </div>
+            </div>-->
+        </section>
+        <section class="bg-white">
+            <div class="container py-5">
+                <h2 class="text-center">The African American Dance Ensemble uses dance and music to bring awareness to the inequities in our community and through our ART, createa positive change.</h2>
+            </div>
+        </section>
+        <section>
+            <div class="container py-5">
+            <h2 class="text-center pb-5">Performing arts * Health equity * Youth empowerment *</h2>
+            <div class="row text-center">
+                <div class="col-lg-4">
+                    <h6 class="text-uppercase">Dance with a purpose</h6>
+                    <p class="text-lg text-muted mb-lg-0">We dance for peace, we dance for strength.
+                    School performances, Board Trainings, Culture Festivals, and many more is where we can enhance community through the Arts.</p>
+                </div>
+                <div class="col-lg-4">
+                    <h6 class="text-uppercase">A healthy you</h6>
+                    <p class="text-lg text-muted mb-lg-0">Through African Dance, learn to become at peace with your body, mind and soul.
+                    A healthy you is a happy you!</p>
+                </div>
+                <div class="col-lg-4">
+                    <h6 class="text-uppercase">Our youth is our future</h6>
+                    <p class="text-lg text-muted mb-lg-0">Teaching our children Peace, Love, Respect, today, for Everybody, is creating a better world through the Arts.</p>
                 </div>
             </div>
         </section>
         <section class="bg-white">
-            <div class="container py-5">
-                <h2 class="text-center">The strength of AADE is the positive, proactive and cooperation between it’s people. To understand the need of the ARTS to make a difference in the lives of the community we serve. We dance for peace, for health and the well-being of all.</h2>
-            </div>
-        </section>
-        <section class="pink-section">
-            <div class="container py-5">
-                <h2 class="text-center"><q>In The Moment Of Crisis, The Wise Build Bridges And The Foolish Build Dams.
-                </q></h2>
-                <address class="text-right">Nigerian Proverb</address>
-            </div>
-            <div class="container pt-3">
-                <div class="row">
-                    <div class="the-african-connection-text-column col-lg-6">
-                        <div class="the-african-connection-text py-5">
-                            <h2 class="text-muted letter-spacing-5 my-4">The African Connection</h2>
-                            <p>Chuck’s connection to Africa began with his birth, but the artistic connection began with a performance of the Sierra Leone National Dance Company at the New York World’s Fair in 1964. Study in Africa became a goal. The opportunity came in 1977 when the Chuck Davis Dance Company participated in FESTAC, an international exposition and celebration of African culture, held in Lagos, Nigeria. Each year thereafter,  he has returned to the continent for further study of traditional dance styles and music.</p>
+            <div>
+                <div class="container">
+                    <div class="row">
+                        <h2 class="text-center">Building Stronger Communities through the power of the ARTS!</h2>
+                        <div class="foundation-column col-lg-4">
+                            <img src="<?php echo get_template_directory_uri();?>/images/dac-logo2.gif" alt="#">
+                        </div>
+                        <div class="foundation-column col-lg-4">
+                            <img src="<?php echo get_template_directory_uri();?>/images/uaclogo.jpg" alt="#">
+                        </div>
+                        <div class="foundation-column col-lg-4">
+                            <img src="<?php echo get_template_directory_uri();?>/images/ncartslogo.jpg" alt="#">
                         </div>
                     </div>
-                    <div class="p-lg-0 the-aade-image-column col-lg-6">
-                        <img src="<?php echo get_template_directory_uri();?>/images/aade-cropwalk-1_orig.jpg" alt="#">
-                    </div>
                 </div>
-                <div class="row">
-                    <div class="p-lg-0 the-aade-column col-lg-6">
-                        <img src="<?php echo get_template_directory_uri();?>/images/531005958.jpg" alt="#">
+            </div>
+        </section>
+        <section>
+            <div class="container bg-blue-light">
+            <div class="row">
+                    <div class="p-lg-0 our-history-image-column col-lg-6">
+                        <div style="display: block; overflow: hidden; position: absolute; inset: 0px; box-sizing: border-box; margin: 0px;">
+                            <img src="<?php echo get_template_directory_uri();?>/images/aade-2_orig.png" alt="#">
+                        </div>
                     </div>
-                    <div class="the-aade-text-column bg-gray-100 col-lg-6">
-                        <div class="the-aade-text py-5">
-                            <h2 class="text-muted letter-spacing-5 my-4">The African American Dance Ensemble</h2>
-                            <p>Each season from 1980 to 1984 the Chuck Davis Dance Company was in residence with the American Dance Festival’s Community Services Program and each season the participants in this outreach program grew. By 1983 it had become obvious that there was a developing core of local dancers whose levels of proficiency were approaching those of the professionals from whom they were learning. The African American Dance Ensemble was born. The Ensemble’s “arrival” was in February 1984 when they gave their first full concert as an independent professional company. In the 1985 season of the American Dance Festival, the African American Dance Ensemble stood on its own and took its place among the other established companies when it premiered two new Chuck Davis works, “Saturday Night, Sunday Morning,” and “Drought,” both of which were commissioned by the American Dance Festival.
+                    <div class="our-mission-text-column bg-gray-100 col-lg-6">
+                        <div class="our-mission-text py-5">
+                            <h2 class="text-muted letter-spacing-5 my-4">Dancers for peace</h2>
+                            <p>AADE is committed to OUTREACH, focusing on the well-being and a possitive future of OUR Youth.
+                            We continue the Legacy of Dr. Chuck Davis in presserving, teaching and performing traditional African Dance Music and Values.
+                            We present dance and music from West African Dance, Music and Values.
+                            We present dance and music from West African Region as well as Congolese Drumming.
+                            Culture, History and Values that promote Unity and Diversity are in all our performances. (Call office for Fee Schedule)
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-    </main>
+   </main>
 </body>
 <?php get_footer(); ?>
