3b5a3087c1036b5f5767c0b764bb3861ed17a0ad
moitorrijos <juanmtorrijos@gmail.com>
2021-05-22 14:33:47 -0500

    Agrega campos personalizados al resto de páginas.

--- page-support-aade.php
+++ page-support-aade.php
@@ -16,53 +16,30 @@
         </div>
         <main>
             <div class="container py-5 bg-gray-100 text-gray-dark">
-                <div class="row">
-                <div class="col-12 col-lg-6 text-center text-lg-left">
-                  <h3 class="font-weight-bold">David-Williams Scholarship</h3>
-                  <p>This scholarship will help students interested in the ARTs, to be able to train and dance with the African American Dance Ensemble and learn the nuances of TRADITIONAL African Dance.</p>
-                  <h3 class="py-3 font-weight-bold">We Collaborate for the betterment of our community We are actively engaged in:</h3>
-                  <ul class="list-unstyled text-lg-left">
-                      <li>ACCESS TO CARE</li>
-                      <li>BLACKSPACE</li>
-                      <li>CONGOLESE DRUMMING</li>
-                      <li>UNLOCKING DOORS INITIATIVE</li>
-                      <li>HEALTH INITIATIVES</li>
-                      <li>VETERANS INITIATIVES</li>
-                      <li>HOUSING & HOMELESS ISSUES</li>
-                  </ul>
-                </div>
-                <div class="col-12 col-lg-6 pt-5 pt-lg-0">
-                  <h3 class="text-center text-lg-left font-weight-bold">KWANZAAFEST -Annual Celebration</h3>
-                  <p class="text-lg-left">Free and Open to the Public for a Community Celebration and coming together in Unity. Honoring our veterans, helping our Homeless and dancing for peace.</p>
-                  <ul class="list-unstyled">
-                      <li>Partners - Individual & Corporate</li>
-                      <li>Choose your level of Partnership</li>
-                      <li>Platinum - $5000.00 +</li>
-                      <li>Diamond - $3000.00 - 4999.99</li>
-                      <li>Gold - $1000.00 - 2999.99</li>
-                      <li>Silver - $500.00 - 999.99</li>
-                  </ul>
-                </div>
+                <div class="row py-5">
+                  <div class="col-6 col-lg-6 text-center text-lg-left">
+                    <?php the_field('column_1'); ?>
+                  </div>
+                  <div class="col-6 col-lg-6">
+                    <?php the_field('column_2'); ?>
+                  </div>
                 </div>
             </div>
             <div class="container-fluid py-5 text-gray-dark">
                 <div class="container">
-                  <h3 class="text-bold text-center">With each partnership you will receive differing levels GREAT packages!</h3>
+                  <?php the_field('strip_1'); ?>
                 </div>
             </div>
             <div class="container py-5 text-center text-gray-dark">
-              <h3 class="text-bold mb-3">Where is my money going?<br>How will it be used?</h3>
-              <p>Great QUESTION!! You can make that choice and determination! Your can choose to support the different activities and parts of the African American Dance Ensemble, But know this, your funds help to keep KWANZZAFEST a free production. It will pay for training and empowering youths in the Arts, Cultural Awareness and Celebration through our OUTREACH. It will also help us to keep "EDUtaining" by the use of traditional dances and songs.</p>
+              <?php the_field('strip_2'); ?>
             </div>
             <div class="container-fluid py-5 text-gray-dark">
               <div class="container">
-                <h3 class="text-bold text-center">Give now and help us help our youth.</h3>
+                <?php the_field('strip_3'); ?>
               </div>
             </div>
             <div class="container py-5 bg-gray-100 text-center text-gray-dark">
-              <h3>ADMINISTRATION * NEW WORKS * TOURING & PERFORMING * EDUCATION</h3>
-              <p>AADE ANNUAL PROGRAMS</p>
-              <p>SCHOLARSHIP FUND</p>
+              <?php the_field('strip_4'); ?>
             </div>
         </main>
     <?php endwhile; ?>
--- page-we-can-dance-for-you.php
+++ page-we-can-dance-for-you.php
@@ -8,7 +8,7 @@
         >
           <div class="container">
             <div class="hero-text mx-auto text-center">
-              <h1 class="text-white text-uppercase my-4 our-history-text">
+              <h1 class="text-white my-4 our-history-text">
                 <?php the_title(); ?>
               </h1>
             </div>
@@ -16,33 +16,26 @@
         </div>
         <main>
             <div class="container">
-              <div class="row text-center d-flex flex-column align-items-center justify-center py-5 px-2 px-md-0">
-                  <a href="#" class="btn btn-lg button-hero text-uppercase bt-bookhere">Book here today!</a>        
-                  <h2 class="text-uppercase text-danger py-4">because of covid-19 all activities are cancelled</h2>
-                  <p>We are available for:    WORKSHOPS, TOURING, CONCERTS, RESIDENCIES, HEALTH INITIATIVES
-                      All of our programs are customized for maximum enjoyment and creativity and audience participation.</p>
+              <div class="row text-center d-flex flex-column align-items-center justify-center py-5">
+                  <a href="<?php the_field('button_url'); ?>" class="btn btn-lg button-hero bt-bookhere px-5"><?php the_field('button_text'); ?></a>
+                  <h2 class="text-danger"><?php the_field('strip_1'); ?></h2>
               </div>
             </div>
             <div class="container-fluid py-5">
-                <div class="container text-center">
-                    <h2 class="text-uppercase">public, private & charter schools  * colleges & universities * corporate & board training & retreats</h2>
+                <div class="container text-center py-5">
+                    <h2><?php the_field('strip_2'); ?></h2>
                 </div>
             </div>
-            <div class="container">
+            <div class="container py-5">
                 <div class="row py-5">
                     <div class="col-12 col-md-4 mb-4 mb-md-0">
-                        <h3 class="text-md-center mb-md-4">Dance Classes</h3>
-                        <p>Monday Nights - 6:30 p.m. - 8:00 p.m. Hayti Heritage Center Instructors - Sis. Ivy and Sis. Toni
-                        Tuessday Nights - 6:30 p.m. - 8:00 p.m. Durham Arts Council - Bro. McDaniel Roberts - (These classes are for those interested in interning with AADE).</p>
+                        <?php the_field('column_1'); ?>
                     </div>
                     <div class="col-12 col-md-4  mb-4 mb-md-0">
-                        <h3 class="text-md-center mb-md-4">Healthy Habits with African Movement Dance your way to Health</h3>
-                        <p>Wednesday Nights - 6:15 p.m. - 7:00p.m. Durham Arts Council - Sis. Britany Burch 
-                        (AADE Exercise and Wellness Program).</p>
+                      <?php the_field('column_2'); ?>
                     </div>
                     <div class="col-12 col-md-4">
-                    <h3 class="text-md-center mb-md-4">Auditions</h3>
-                        <p>If you are interested in dancing with the African American Dance Ensemble, Auditions will be held in July 2020 for the AADE Upcoming Season. Qualifications will be posted on line in April.</p>
+                      <?php the_field('column_3'); ?>
                     </div>
                 </div>
             </div>
